1c95d8c8662fafeb2da8f10fd07bc9072bb39454
Marc Laventure <marclave@uvic.ca>
2017-01-06 16:44:57 -0800

    Added Decoding algorithm

--- Spring2016/UVic/ELEC405/Hamming74.c
+++ Spring2016/UVic/ELEC405/Hamming74.c
@@ -3,6 +3,9 @@
 
   Developed by: Marc Laventure
 
+  List of TODOs:
+    - Pass/return byte (no need for an array, all we need is 8 bits MAX)
+    - Unit tests
  */
 
 #include <stdlib.h>
@@ -11,6 +14,27 @@
 #define ORIGINAL_MESSAGE_SIZE 4
 #define ENCODED_MESSAGE_SIZE 7
 #define DECODED_MESSAGE_SIZE 7
+
+int *Hamming74_decode(int pMessage[], uint8_t pCorrection)
+{
+  // This is our "syndrome" generation
+  int *ret = malloc(sizeof(int *) * DECODED_MESSAGE_SIZE);
+  for (int i = 0; i < ORIGINAL_MESSAGE_SIZE; i++)
+  {
+    ret[i] = pMessage[i];
+  }
+  ret[4] = (pMessage[1] + pMessage[2] + pMessage[3] + pMessage[4]) % 2;
+  ret[5] = (pMessage[0] + pMessage[1] + pMessage[3] + pMessage[5]) % 2;
+  ret[6] = (pMessage[0] + pMessage[2] + pMessage[3] + pMessage[6]) % 2;
+
+  // We may not want to perform the correction, so its conditional
+  if (pCorrection)
+  {
+    // TODO: Correction I might want here, but will de-couple for now
+  }
+
+  return ret;
+}
 
 int *Hamming74_encode(int pMessage[])
 {
@@ -25,7 +49,7 @@ int *Hamming74_encode(int pMessage[])
   return ret;
 }
 
-void outputMessages(int pOriginal[], int pEncoded[])
+void outputMessages(int pOriginal[], int pEncoded[], int pDecoded[])
 {
   for (int i = 0; i < ORIGINAL_MESSAGE_SIZE; i++)
   {
@@ -36,6 +60,11 @@ void outputMessages(int pOriginal[], int pEncoded[])
   {
     printf("%d ", pEncoded[i]);
   }
+  printf("\n");
+  for (int i = 0; i < DECODED_MESSAGE_SIZE; i++)
+  {
+    printf("%d ", pDecoded[i]);
+  }
   printf("\ndone\n");
 }
 
@@ -44,9 +73,14 @@ int main(void)
   // This is for testing purposes at the moment.
   int message[] = {0, 0, 1, 0};
   int *sentMessage;
+  int *decodedMessage;
+
   sentMessage = Hamming74_encode(message);
-  outputMessages(message, sentMessage);
+  decodedMessage = Hamming74_decode(sentMessage, 0);
+  outputMessages(message, sentMessage, decodedMessage);
 
   free(sentMessage);
+  free(decodedMessage);
+
   return 0;
 }
